24c31aef4b4ef6179913b1373bd1d7117fe1124b
Bluskyfishing <blusky445@gmail.com>
2024-01-17 14:34:14 +0100

    added comments and fixed 0 print. #6

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -8,11 +8,11 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Isak"); //prints name 
-            CountTo(11); // counts to 10 from 0 
+            CountTo(11); // counts to 10 from 1 
         }
         static void CountTo(int num)
         {
-            for (int i = 0; i < num; i++) // loops through argument.
+            for (int i = 1; i < num; i++) // changed to 1 instead of 0, to avoid 0 print. 
             {
                 Console.WriteLine(i);
             }
